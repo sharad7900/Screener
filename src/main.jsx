2bced553afb0b498bb225bef0146dde84a55d920
mongodb <mongodbid67@gmail.com>
2025-07-29 21:10:49 +0530

    Cols Adding

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import { Provider  } from "./components/ui/provider"
+import { Provider } from "../src/Components/ui/provider.jsx"
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'

8f3326f0c2a0b909d1422ce1f55ee8fc3811703b
mongodb <mongodbid67@gmail.com>
2025-10-24 16:31:13 +0530

    Multicap

--- src/Components/Filters.jsx
+++ src/Components/Filters.jsx
@@ -26,7 +26,7 @@ function useDebounce(value, delay = 400) {
 
 const assetClassOptions = ["Equity", "Debt", "Solution Oriented", "Other", "Hybrid"];
 const categoryOptions = [
-  "Large Cap", "Mid Cap", "Small Cap", "Flexi Cap", "Retirement", "Contra", "Value", "Focused", "Banking and PSU", "FoF", "Multi Asset", "Sectoral: Auto", "Sectoral: Other"
+  "Large Cap", "Mid Cap", "Small Cap", "Multi Cap","Flexi Cap", "Retirement", "Contra", "Value", "Focused", "Banking and PSU", "FoF", "Multi Asset", "Sectoral: Auto", "Sectoral: Other"
 ];
 const MenuProps = {
   PaperProps: { style: { maxHeight: 220, width: 250 } }
